60199b05ff29aed762a06513bcf416618bccb682
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2020-07-10 10:39:16 -0500

    cleanup

--- jenkins/init.groovy
+++ jenkins/init.groovy
@@ -6,24 +6,40 @@ import jenkins.security.apitoken.*
 import jenkins.install.*;
 import hudson.util.*;
 
-// create token
+println "create token..."
+try {
+    // create token
 
-// script parameters
-def userName = 'ecrdb'
-def tokenName = 'ecrdb-token'
+    // script parameters
+    def userName = 'ecrdb'
+    def tokenName = 'ecrdb-token'
 
-def user = User.get(userName, false)
-def apiTokenProperty = user.getProperty(ApiTokenProperty.class)
-def result = apiTokenProperty.tokenStore.generateNewToken(tokenName)
+    def user = User.get(userName, false)
+    def apiTokenProperty = user.getProperty(ApiTokenProperty.class)
+    def result = apiTokenProperty.tokenStore.generateNewToken(tokenName)
 
-user.save()
+    user.save()
 
-println "Got token: $result.plainValue"
+    println "Got token: $result.plainValue"
+    
+    File file = new File("/var/jenkins_home/secrets/ecrdb_token.txt")
+    file.write result.plainValue
 
-File file = new File("/var/jenkins_home/secrets/ecrdb_token.txt")
-file.write result.plainValue
+    //new File("/var/jenkins_home/secrets/ecrdb_token.txt").withWriter('utf-8') { 
+    //    writer -> writer.writeLine $result.plainValue 
+    //}  
+
+
+} catch(Exception e) {
+    println "exception on something occurred "+e
+    System.exit(0)
+}
+
+
+
+
 //return result.plainValue
-
+println "getting docker binary..."
 // get docker binary
 def cmd = ["/get_docker_binary.sh"]
 
@@ -42,3 +58,5 @@ cmd.execute().with{
 // from https://riptutorial.com/jenkins/example/24925/disable-setup-wizard
 def instance = Jenkins.getInstance()
 instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+
+println "init.groovy is done"
