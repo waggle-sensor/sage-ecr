f05880d5eb5bc7d2a572c34421124cf0445ac09d
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-01-25 09:27:45 -0600

    updated Dockerfile and using fixed token in init script

--- jenkins/init.groovy
+++ jenkins/init.groovy
@@ -6,54 +6,16 @@ import jenkins.security.apitoken.*
 import jenkins.install.*;
 import hudson.util.*;
 
-println "create token..."
-try {
-    // create token
+println "setting up ecr token..."
+def userName = 'ecrdb'
+def tokenName = 'ecrdb-token'
+// TODO(sean) read this from secret
+def tokenValue = '114df297873d76b7865486b00e28afa881'
 
-    // script parameters
-    def userName = 'ecrdb'
-    def tokenName = 'ecrdb-token'
-
-    def user = User.get(userName, false)
-    def apiTokenProperty = user.getProperty(ApiTokenProperty.class)
-    def result = apiTokenProperty.tokenStore.generateNewToken(tokenName)
-
-    user.save()
-
-    println "Got token: $result.plainValue"
-    
-    File file = new File("/var/jenkins_home/secrets/ecrdb_token.txt")
-    file.write result.plainValue
-
-    //new File("/var/jenkins_home/secrets/ecrdb_token.txt").withWriter('utf-8') { 
-    //    writer -> writer.writeLine $result.plainValue 
-    //}  
-
-
-} catch(Exception e) {
-    println "exception on something occurred "+e
-    System.exit(0)
-}
-
-
-
-
-//return result.plainValue
-println "getting docker binary..."
-// get docker binary
-def cmd = ["/get_docker_binary.sh"]
-
-cmd.execute().with{
-    def output = new StringWriter()
-    def error = new StringWriter()
-    //wait for process ended and catch stderr and stdout.
-    it.waitForProcessOutput(output, error)
-    //check there is no error
-    println "error=$error"
-    println "output=$output"
-    println "code=${it.exitValue()}"
-}
-
+def user = User.get(userName, false)
+def apiTokenProperty = user.getProperty(ApiTokenProperty.class)
+// TODO(sean) check if fails if already exists
+apiTokenProperty.tokenStore.addFixedNewToken(tokenName, tokenValue)
 
 // from https://riptutorial.com/jenkins/example/24925/disable-setup-wizard
 def instance = Jenkins.getInstance()
